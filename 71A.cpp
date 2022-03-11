bad0a738cf016205e32cb0de64d3556111de3b6a
JonJovinsson1 <73032115+JonJovinsson1@users.noreply.github.com>
2022-03-11 08:53:57 +0000

    Create 71A.cpp

---
+++ 71A.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+    while(n){
+        string s;
+        cin >> s;
+        if(s.size()<=10){
+            cout << s << endl;
+        }
+        else if(s.size()>10 and s.size()<=100){
+            cout << s[0] << s.size()-2 << s[s.size()-1] << endl;
+        } 
+    n--;
+    
+    }
+	return 0;
+}

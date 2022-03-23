de970a68f39a13001288791db60bd998bf402725
JonJovinsson1 <73032115+JonJovinsson1@users.noreply.github.com>
2022-03-23 09:19:46 +0000

    Create 136A.cpp

---
+++ 136A.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main(){
+    int n,k;
+    cin>>n;
+    int arr[n+1];
+    for(int i=1;i<=n;i++){
+        cin>>k;
+        arr[k]=i;
+    }
+    for(int i=1;i<=n;i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+    }
